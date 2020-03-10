bdb68eff878a7f790fb42e4a2b64777f83ee154b
mtktauseef <mtktauseef@gmail.com>
2020-03-10 12:56:39 +0000

    Add files via upload

---
+++ src/main/java/com/taustek/demoservicecatalog/DemoServiceCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.taustek.demoservicecatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoServiceCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoServiceCatalogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/taustek/demoservicecatalog/DemoServiceCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taustek.demoservicecatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoServiceCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
